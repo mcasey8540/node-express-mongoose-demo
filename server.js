4e0e6e4d7b9ac71be467acbfdb882a12196f349e
Michael Casey <mcasey8540@gmail.com>
2016-08-23 11:04:03 -0400

    Update server.js with License info

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 /*
  * nodejs-express-mongoose-demo
- * Copyright(c) 2013 Madhusudhan Srinivasa <madhums8@gmail.com>
+ * Copyright(c) 2016 Michael Caset <mcasey8540@gmail.com>
  * MIT Licensed
  */
 
